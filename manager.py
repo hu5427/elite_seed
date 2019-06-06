09e9540ee7ea5a5aff8859086f430f40ae7558fa
qizi <hu5427@163.com>
2019-06-07 01:05:58 +0800

    集成flask_session

--- manager.py
+++ manager.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
+from flask_session import Session
 
 app = Flask(__name__)
 
 
 # 创建配置类
 class Config(object):
+    SECRET_KEY = 'dictionary'
     DEBUG = True
 
     # 配置数据库
@@ -17,6 +19,21 @@ class Config(object):
     # 集成redis
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = '6379'
+
+    # flask_session 配置信息
+
+    # 1.储存方式指定为redis
+    SESSION_TYPE = "redis"
+    # 2.设置session签名 加密
+    SESSION_USE_SIGNER = True
+    # 3.设置存储对象
+    SESSION_REDIS = StrictRedis(
+        host=REDIS_HOST,port=REDIS_PORT)
+    # 4.设置session为永久保存
+    SECRET_PERMANENT = False
+    # 5.设置存储有效期为一天（单位是秒）
+    PERMANENT_SESSION_LIFETIME = 8640
+
 
 
 # 一 配置DEBUG
@@ -33,6 +50,9 @@ redis_store = StrictRedis(
 
 # 四 配置CSRF
 CSRFProtect(app)
+
+# 五 集成flask_session
+Session(app)
 
 
 @app.route('/')
