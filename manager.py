00adaeaab5dc8cefef6e4e425fa0802b9108f7d8
qizi <hu5427@163.com>
2019-06-07 00:44:20 +0800

    集成redis

--- manager.py
+++ manager.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 app = Flask(__name__)
 
 
-# 一 创建配置类
+# 创建配置类
 class Config(object):
     DEBUG = True
 
@@ -12,10 +13,22 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1/elite_seed"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # 集成redis
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = '6379'
 
+
+# 一 配置DEBUG
 app.config.from_object(Config)
+
 # 二 集成SQLAlchemy到flask
 db = SQLAlchemy(app)
+
+# 三 集成redis
+redis_store = StrictRedis(
+    host=Config.REDIS_HOST,
+    port=Config.REDIS_PORT
+)
 
 
 @app.route('/')
