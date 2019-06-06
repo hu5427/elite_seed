02218285a732b042e970d708134d3be48a6f82b8
qizi <hu5427@163.com>
2019-06-07 00:35:43 +0800

    集成SQLAlchemy到flask

--- manager.py
+++ manager.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# 创建配置类
-class Config():
+
+# 一 创建配置类
+class Config(object):
     DEBUG = True
+
+    # 配置数据库
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1/elite_seed"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app.config.from_object(Config)
+# 二 集成SQLAlchemy到flask
+db = SQLAlchemy(app)
 
 
 @app.route('/')
