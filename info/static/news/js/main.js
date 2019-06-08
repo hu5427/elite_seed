3d8e39214e6b2c7d861012175fae36d94e5be5b6
qizi <hu5427@163.com>
2019-06-08 14:52:15 +0800

    蹄片验证码前端实现

--- info/static/news/js/main.js
+++ info/static/news/js/main.js
@@ -152,7 +152,11 @@ var imageCodeId = ""
 
 // TODO 生成一个图片验证码的编号，并设置页面中图片验证码img标签的src属性
 function generateImageCode() {
-
+    // 生成随机uuid
+    imageCodeId = generateUUID()
+    var url = "/img_code?imageCodeID=" + imageCodeId
+    // 替换img属性
+    $(".get_pic_code").attr("src", url)
 }
 
 // 发送短信验证码
