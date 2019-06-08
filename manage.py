bdd31fa02348c227468ee8e6bf99c4f537d4a0e9
qizi <hu5427@163.com>
2019-06-08 11:28:29 +0800

    导入model.py,数据库迁移

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_migrate import MigrateCommand, Migrate
 from flask_script import Manager
 from info import create_app, db
+from info.models import *
 
 # 调用create_app函数将配置类传入函数
 app = create_app("development")
